9843ccd68bcc9ff984eca5698b505d204f99ad49
gitname <gitname>
2018-11-11 21:22:29 +0800

    添加test到git2

--- get-demo/src/com/get/demo/GetTest.java
+++ get-demo/src/com/get/demo/GetTest.java
@@ -3,5 +3,6 @@ package com.get.demo;
 public class GetTest {
     public static void main(String[] args) {
         System.out.println("aaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaa");
     }
 }
